b3300954b9e213a3cea649f4087a31fe77c17f0c
joselucas257 <69659496+joselucas257@users.noreply.github.com>
2020-08-14 19:09:09 -0300

    Feramentas
    E nois

---
+++ main.py
@@ -0,0 +1,69 @@
+#-*-coding:utf8;-*-
+#qpy:console
+import sys
+import os
+import time
+import socket
+import random
+print()
+print()
+print ()
+print ("BEM VINDO A")
+print()
+print()
+time.sleep (1)
+print ()
+print ()
+print ("===========================")
+print ("        |D.H.C_team|       ")
+print ("===========================")
+print 
+time.sleep (2)
+print ()
+print ("CRIADO POR: jose lucas")
+print ()
+time.sleep (1)
+print ()
+print()
+print ()
+time.sleep (2)
+nome = input ('Qual o seu nome?')
+
+print ()
+time.sleep (2)
+
+print ('ESSE E O CORNO >>> ',nome,)
+print ()
+time.sleep (3)
+print ()
+time.sleep (3)
+print ()
+print ()
+print ("####################################")
+p=input ('voce deseja ir as ferramentas?')
+if p=='sim':
+    time.sleep (3)
+time.sleep (2)
+
+print ("======================")
+print ()
+print ("[1]attack ddos")
+print ("[2]em breve")
+print ("[3]em breve")
+print ("======================")
+print()
+
+time.sleep (8)
+print ("===========CAREGANDO=============")
+print ("[*]••••                       10%")
+time.sleep (2)
+print ("[*]•••••••••                  20%")
+time.sleep (1)
+print ("[*]•••••••••••••••            50%")
+time.sleep (2)
+print ("[*]••••••••••••••••••••       70%")
+time.sleep (3)
+print ("[*]••••••••••••••••••••••••• 100%")
+print ("=================================")
+
+os.system("git clone https://github.com/joselucas257/D.H.C-DDos")
